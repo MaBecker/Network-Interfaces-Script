e640a137302f41e1ba4f6387fbccaf3d3ff512f8
MaBecker <mark.becker@sbit-gmbh.de>
2016-10-22 13:18:56 +0200

    to work with node v0.12.15
    * cost to var
    * => to function
    * ` to '

--- src/script-runner.js
+++ src/script-runner.js
@@ -1,33 +1,33 @@
 'use strict';
 
-const exec = require('child_process').exec;
-const path     = require('path');
+var exec = require('child_process').exec;
+var path     = require('path');
 
-const READ_SCRIPT_PATH  = path.join(__dirname, './awk/readInterfaces.awk');
-const WRITE_SCRIPT_PATH = path.join(__dirname, './awk/changeInterface.awk');
-const DEFAULT_INTERFACE = 'eth0';
-const DEFAULT_INTERFACE_FILE_LOCATION = '/etc/network/interfaces';
-const ADDRESS_RETURN_ORDER = ['address', 'netmask', 'network', 'gateway', 'powersave'];
+var READ_SCRIPT_PATH  = path.join(__dirname, './awk/readInterfaces.awk');
+var WRITE_SCRIPT_PATH = path.join(__dirname, './awk/changeInterface.awk');
+var DEFAULT_INTERFACE = 'eth0';
+var DEFAULT_INTERFACE_FILE_LOCATION = '/etc/network/interfaces';
+var ADDRESS_RETURN_ORDER = ['address', 'netmask', 'network', 'gateway', 'powersave'];
 
 
-const convertArgsForScript = function convertArgsForScript (interfacesFilePath, args) {
-  const converted = [interfacesFilePath];
+var convertArgsForScript = function convertArgsForScript (interfacesFilePath, args) {
+  var converted = [interfacesFilePath];
 
   // The script requires that device be specified as 
   // "dev=<deviceName>" if changing the interface
-  converted.push(`dev=${args.device}`);
+  converted.push('dev=${args.device}');
   delete args.device;
 
   for (let key in args) {
-    converted.push(`${key}=${args[key]}`);
+    converted.push('${key}=${args[key]}');
   }
 
   return converted.join(' ');
 }
 
-const formatResult = function formatResult (textResult) {
-  const rawResult  = textResult.trim().split(' ');
-  const formattedResult = {};
+var formatResult = function formatResult (textResult) {
+  var  rawResult  = textResult.trim().split(' ');
+  var  formattedResult = {};
 
   if (rawResult[0] === 'dhcp' || rawResult[0] === 'manual') {
     formattedResult.mode = rawResult[0];
@@ -40,9 +40,9 @@ console.log(formattedResult);
   return formattedResult;
 };
 
-const writeToFile = function writeToFile(filePath, content) {
-  return new Promise((resolve, reject) => {
-    exec(`echo "${content}" | tee  ${filePath} > /dev/null`, (error, stdout, stderr) => {
+var writeToFile = function writeToFile(filePath, content) {
+  return new Promise(function(resolve, reject) {
+    exec('echo "${content}" | tee  ${filePath} > /dev/null', function(error, stdout, stderr) {
       if (error) {
         reject(error);
       } else {
@@ -52,9 +52,9 @@ const writeToFile = function writeToFile(filePath, content) {
   });
 };
 
-const runScript = function runScript (scriptName, args) {
-	return new Promise((resolve, reject) => {
-		const child = exec(`awk -f ${scriptName} ${args}`, (error, stdout, stderr) => {
+var runScript = function runScript (scriptName, args) {
+	return new Promise(function(resolve, reject) {
+		const child = exec('awk -f ${scriptName} ${args}', function(error, stdout, stderr) {
 		  if (error) {
 		    reject(error);
 		  }
@@ -63,41 +63,41 @@ const runScript = function runScript (scriptName, args) {
 	});
 };
 
-const read = function read (interfacesFilePath, interfaceName) {
+var read = function read (interfacesFilePath, interfaceName) {
   interfacesFilePath = interfacesFilePath || DEFAULT_INTERFACE_FILE_LOCATION;
   interfaceName = interfaceName || DEFAULT_INTERFACE;
 
-  return new Promise((resolve, reject) => {
-    runScript(READ_SCRIPT_PATH, [interfacesFilePath, `device=${interfaceName}`].join(' '))
-      .then((success) => {
+  return new Promise(function(resolve, reject) {
+    runScript(READ_SCRIPT_PATH, [interfacesFilePath, 'device=${interfaceName}'].join(' '))
+      .then(function(success) {
         console.log(success);
          resolve(formatResult(success));
-      }).catch((error) => {
+      }).catch(function(error) {
         reject(error);
       })
   });
   
 };
 
-const write = function write (interfacesFilePath, interfaceName, args) {
+var write = function write (interfacesFilePath, interfaceName, args) {
   interfacesFilePath = interfacesFilePath || DEFAULT_INTERFACE_FILE_LOCATION;
   interfaceName = interfaceName || DEFAULT_INTERFACE;
 
-  return new Promise ((resolve, reject) => {
+  return new Promise (function(resolve, reject) {
     args.device = interfaceName;
     const formattedArgs = convertArgsForScript(interfacesFilePath, args);
 
     runScript(WRITE_SCRIPT_PATH, formattedArgs)
-      .then((newText) => {
-        writeToFile(interfacesFilePath, newText).then((success) => {
+      .then(function(newText) {
+        writeToFile(interfacesFilePath, newText).then(function(success) {
           resolve(args);
         })
-        .catch((fileWriteError) => {
+        .catch(function(fileWriteError) {
           reject(fileWriteError);
         })
       })
-      .catch((error) => {
-        console.log(`Error running script: ${error}`);
+      .catch(function(error) {
+        console.log('Error running script: ${error}');
         reject(error);
       });
   });
